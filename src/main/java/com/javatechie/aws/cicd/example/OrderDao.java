cfc20e3be2b6ed4f8ecec4cf2a269b1ad589b8d1
MCL <modupe18ab@gmail.com>
2025-04-21 21:23:31 -0500

    Update OrderDao.java

--- src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
@@ -14,7 +14,7 @@ public class OrderDao {
         return Stream.of(
                 new Order(101, "Mobile", 1, 30000),
                 new Order(58, "Book", 4, 2000),
-                new Order(205, "Laptop", 1, 150000),
+                new Order(205, "AppleLaptop", 1, 150000),
                 new Order(809, "headset", 1, 1799),
                 new Order(809, "Mouse", 3, 499),
                 new Order(809, "Digitalbooks", 24, 1499),
